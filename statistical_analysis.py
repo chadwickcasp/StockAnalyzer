88de6dedd8456b1c260b9bd950ba5a86568ce62d
chadwickcasp <casperslide@berkeley.edu>
2018-02-01 09:55:33 -0800

    Statistical Analysis added

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -31,17 +31,41 @@ def n_grams(tokens, n):
 n_analyzed = 30
 month_list = [((now.month + i-1)%12+1, int(floor((now.month+i)/13)+now.year-1)) for i \
               in range(number_of_months+1)]
+for ticker in tickers:
+    overall_diffs = pd.DataFrame()
+    for month, year in month_list:
+        print(month, year)
+        try:
+            df = pd.read_csv('./Data/NYTimesArticlesAndStocks('+ticker_string_list+')_{0}-{1}.csv'.format(month,year))
+        except IOError as e:
+            print("Month data not found in directory...")
+            print("Check list and try again")
+            break
+        # df = df.drop_duplicates('_id')
+        df = df.reset_index()
+        # print(df[u'ticker'].drop_duplicates())
+        df = df[df['ticker'] == ticker]
+        # print(df)
+        # data_shape = df.shape
+        # length = data_shape[0]
+        # # docs = [df.ix[i] for i in range(length)]
+        # bigrams_dict = {}
+        # trigrams_dict = {}
+        # grams_dict = {}
+        print(df.columns)
+        diffs = pd.DataFrame()
+        diffs['date'] = df['date'].drop_duplicates()
+        diffs['close'] = df['close'].drop_duplicates()
+        overall_diffs = overall_diffs.append(diffs)
 
-for month, year in month_list[0]:
-    print(month, year)
-    df = pd.read_csv('./Data/NYTimesArticlesAndStocks('+ticker_string_list+')_{0}-{1}.csv'.format(month,year))
-    df = df.drop_duplicates('_id')
-    df = df.reset_index()
-    data_shape = df.shape
-    length = data_shape[0]
-    docs = [df.ix[i] for i in range(length)]
-    bigrams_dict = {}
-    trigrams_dict = {}
-    grams_dict = {}
-    print(df.columns)
+    print(overall_diffs)
+    print(overall_diffs.columns)
+    overall_diffs = overall_diffs.sort_values('date')
+    overall_diffs['diffs'] = (overall_diffs['close'] - overall_diffs['close'].shift(-1))
+    overall_diffs['diffs'] = overall_diffs['diffs'].divide(overall_diffs['close'])*100
+    print(overall_diffs)
+    plt.figure()
+    overall_diffs.hist(column='diffs', alpha=0.7, bins=50)
+    plt.title("Frequency of Day-to-day Percent Changes of {}".format(ticker))
+plt.show()
     # for i, article in enumerate(docs):
